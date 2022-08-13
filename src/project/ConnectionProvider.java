8bdd6c54d7cb57da013e7eb4fe41387fa23d9fe6
Aishatarek <aishatarek83@gmail.com>
2022-08-13 19:16:45 +0200

    first commit

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+/**
+ *
+ * @author hp
+ */
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/gym","root","ai3esha123");
+            return con;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
